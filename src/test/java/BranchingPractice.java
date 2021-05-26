cb474b4da7de4ac9e845b290966a5d3b7b6c67eb
KunduzKoyu <kunduzazamat@gmail.com>
2021-05-25 21:14:51 -0500

    Branching Practice more work done

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,5 +1,6 @@
 public class BranchingPractice {
     public static void main(String[] args) {
         System.out.println("Branching Practice started");
+        System.out.println("More commit");
     }
 }
